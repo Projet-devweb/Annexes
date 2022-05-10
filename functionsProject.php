5ce3808a3b63b9e9583ce5f4e18cdace25303ceb
BOupdown <75842770+BOupdown@users.noreply.github.com>
2022-05-10 12:39:27 +0200

    Add files via upload

---
+++ functionsProject.php
@@ -0,0 +1,17 @@
+<?php
+function randomDate($start_date, $end_date)
+{
+  // Convert to timetamps
+  $min = strtotime($start_date);
+  $max = strtotime($end_date);
+
+  // Generate random number using above bounds
+  $val = rand($min, $max);
+
+  // Convert back to desired date format
+  return date('d/m/Y', $val);
+}
+
+
+
+?>
