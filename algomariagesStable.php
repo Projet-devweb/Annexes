9b465a658e1ba741d22a71144996963180247dd7
mamertoo <83581097+mamertoo@users.noreply.github.com>
2022-05-18 09:21:42 +0200

    Add files via upload

--- algomariagesStable.php
+++ algomariagesStable.php
@@ -8,32 +8,25 @@
 
 	<body>
     <?php
-    //fonction qui prend en parametre un tab, le nb de colonnes de ce tab et la ligne qu'on veut copier et qui renvoie un tab contenant toute la ligne que l'on veut copier
-    function copierligne($tab,$num,$ligne){
-      for ($i=0; $i <$num ; $i++) {
-
-      }
-
-    }
-    function triABulle($monTab,$num){
+/*
+    function triABulle($tab,$num){
              $cpt=true;
              while ($cpt){
                    $cpt=false;
-                   for ($i=0;$i<sizeof($monTab)-1;$i++){
-                       if ($monTab[$i][4]> $monTab[$i+1][4]){
-                          $temp=$monTab[$i+1][4];
-                          $monTab[$i+1][4]=$monTab[$i][4];
-                          $monTab[$i][4]=$temp;
+                   for ($i=0;$i<sizeof($tab)-1;$i++){
+                       if ($tab[$i][4]> $tab[$i+1][4]){
+                          $temp=$tab[$i+1][4];
+                          $tab[$i+1][4]=$tab[$i][4];
+                          $tab[$i][4]=$temp;
                           $cpt=true;
     		    }
                    }
              }
-             return $monTab;
-    }
-
+             return $tab;
+    }*/
     $row = 1;
     $tab=array();
-    if (($handle = fopen("choixEtudiantsParcours2.csv", "r")) !== FALSE) {
+    if (($handle = fopen("choixEtudiantsParcours3.csv", "r")) !== FALSE) {
         while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
           $tab[]=$data;
             $num = count($data);// $data contient les lignes
@@ -44,19 +37,107 @@
             }
 
         }
-        echo $tab[0][4]."<br>";
-        $tabtrie=triABulle($tab,$num);
-        for ($i=0; $i < $row; $i++) {
+				unset($tab[0]);// on enleve la premiere ligne correspondant a l'entete
+				$cpt=true;
+				while ($cpt){
+							$cpt=false;
+							for ($i=0;$i<sizeof($tab)-1;$i++){
+									if ((float)$tab[$i][4]> (float)$tab[$i+1][4]){
+										 $temp=$tab[$i+1];
+										 $tab[$i+1]=$tab[$i];
+										 $tab[$i]=$temp;
+										 $cpt=true;
+
+			 }
+							}
+				}
+
+				$cpt=true;
+				while ($cpt){
+							$cpt=false;
+							for ($i=0;$i<sizeof($tab)-1;$i++){//si les moyennes sont les memes on trie selon les ectcs
+									if ((float)$tab[$i][4]== (float)$tab[$i+1][4]&& (float)$tab[$i][3]> (float)$tab[$i+1][3] ){
+										 $temp=$tab[$i+1];
+										 $tab[$i+1]=$tab[$i];
+										 $tab[$i]=$temp;
+										 $cpt=true;
+
+			 }
+							}
+				}
+				$tab=array_reverse($tab);
+				$premiere=$tab[0];//la premiere ligne cause probleme donc on la trie manuelement
+
+				//print_r($tab);
+
+        /*for ($i=0; $i < $row; $i++) {
           for ($j=0; $j <$num; $j++) {
-          echo $tabtrie[$i][$j]." ";
+          echo $tab[$i][$j]." ";
           }
           echo "<br>";
 
-        }
+        }*/
         fclose($handle);
     }
 
-    //utiliser array_multisort() et array_column()
+		function mariagesstables($tab,$num,$row){//row doit etre le nb de colonnes du tab
+			$options=array();
+			switch ($num) {
+				case 11:
+				$options[0][0]='CS';//nom des filieres
+				$options[1][0]=70;//places disponibles
+
+				$options[0][1]='IAC';
+				$options[1][1]=50;
+
+				$options[0][2]='VISUA';
+				$options[1][2]=35;
+
+				$options[0][3]='ICC';
+				$options[1][3]=35;
+
+				$options[0][4]='INEM';
+				$options[1][4]=35;
+
+				$options[0][5]='BI';
+				$options[1][5]=20;
+
+				$options[0][6]='HPDA';
+				$options[1][6]=18;
+
+				$options[0][7]='IAP';
+				$options[1][7]=17;
+
+
+
+				for ($i=0; $i <2 ; $i++) {
+					for ($j=0; $j <8 ; $j++) {
+						echo $options[$i][$j]." ";
+					}
+					echo "<br>";
+				}
+				$fin=false;
+					for ($i=0; $i <$row ; $i++) {
+						for ($j=0; $j <$num ; $j++) {
+							echo $tab[$i][$j]." ";
+
+						}
+						echo "<br>";
+					}
+
+
+
+					break;
+
+				default:
+					break;
+			}
+
+		}
+		//echo $num;
+		mariagesstables($tab,$num,$row);
+
+
     ?>
 
 	</body>
